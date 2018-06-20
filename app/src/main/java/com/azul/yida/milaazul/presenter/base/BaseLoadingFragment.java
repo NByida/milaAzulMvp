4ca6fc31ff4c068b0320d53b549b93de2aad2bcf
一沓 <1582742921@qq.com>
2018-06-20 17:47:55 +0800

    尝试封装一个自定义加载的progress控件

--- app/src/main/java/com/azul/yida/milaazul/presenter/base/BaseLoadingFragment.java
+++ app/src/main/java/com/azul/yida/milaazul/presenter/base/BaseLoadingFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.azul.yida.milaazul.R;
 import com.azul.yida.milaazul.common.Mlog;
@@ -17,16 +19,18 @@ public class BaseLoadingFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Dialog dialog=new Dialog(getActivity());
         dialog.setContentView(R.layout.loading_dialog);
-        circleProgressBar=dialog.findViewById(R.id.progress_bar);
-        circleProgressBar.setProgress(50);
+        dialog.setCanceledOnTouchOutside(false);
         dialog.setCancelable(false);
         Mlog.t("onCreateDialog");
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.dimAmount = 0f;
+        window.setAttributes(params);
+        dialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_transparent));
         return dialog;
+
     }
 
-//    public void show(FragmentManager fm) {
-//        this.show(fm, "LoadingDialogFragment");
-//    }
 
 
 }
--- app/src/main/java/com/azul/yida/milaazul/view/MainActivityView.java
+++ app/src/main/java/com/azul/yida/milaazul/view/MainActivityView.java
@@ -1,34 +1,65 @@
 package com.azul.yida.milaazul.view;
 
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.azul.yida.milaazul.R;
 import com.azul.yida.milaazul.common.Mlog;
+import com.azul.yida.milaazul.presenter.base.BaseConfirmFragment;
+import com.azul.yida.milaazul.presenter.base.BaseLoadingFragment;
 import com.azul.yida.milaazul.view.base.MvpView;
-import com.azul.yida.milaazul.view.myView.CircleProgressBar;
 
 import butterknife.BindView;
-import butterknife.BindViews;
 import butterknife.OnClick;
 
 public class MainActivityView extends MvpView {
 
+    @BindView(R.id.lay_content)
+    ViewGroup layContent;
+
     @OnClick(R.id.bt_show)
     public void showDialog(){
-        showLoading();
+        //showLoading();
         //setProgress();
+        startLoading();
+        //new BaseLoadingFragment().show(((AppCompatActivity)getActivity()).getSupportFragmentManager(),"");
         Mlog.t("showDialog");
     }
 
     @OnClick(R.id.bt_dissmiss)
     public void dissMiss(){
-       dissmissLoading();
+        stoptLoading();
     }
     @Override
     public int getLayoutId() {
         return R.layout.main_activity;
     }
 
+    public void startLoading(){
+        for(int a=0;a<layContent.getChildCount();a++){
+            layContent.getChildAt(a).setVisibility(View.GONE);
+        }
+        LayoutInflater layoutInflater=getActivity().getLayoutInflater();
+        View loadView=layoutInflater.inflate(R.layout.loading_dialog,null);
 
+        RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+       p.addRule(RelativeLayout.CENTER_IN_PARENT);
+        layContent.addView(loadView,p);
+    }
+
+    public void stoptLoading(){
+//        View loadView=layContent.findViewById(R.id.progress_bar);
+//        if(loadView!=null){
+            layContent.removeView(layContent.getChildAt(layContent.getChildCount()-1));
+//        }
+        for(int a=0;a<layContent.getChildCount();a++){
+            layContent.getChildAt(a).setVisibility(View.VISIBLE);
+        }
+    }
 
 }
